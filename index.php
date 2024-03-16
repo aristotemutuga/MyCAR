038722f78387c038d914ed9f45dc45e64d8f95bf
jennermaxim <jennersi1remaxim@gmail.com>
2024-03-16 22:22:51 +0300

    Add form index index file

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php include 'include/header.php';?>
+<?php include 'include/header.php'; ?>
+
+<div class="login">
+    <form action="">
+        <input type="image" src="" alt="">
+    </form>
+</div>
+
+<?php include 'include/footer.php'; ?>
