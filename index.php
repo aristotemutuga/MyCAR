4bc0220032c5ab1526469247d98d5370c68deea9
jennermaxim <jennersi1remaxim@gmail.com>
2024-03-17 13:49:58 +0300

    Add the cars on the home page

--- index.php
+++ index.php
@@ -4,6 +4,111 @@
     <div class="cars-container">
         <div class="car-details">
             <img src="assets/images/car.jpg" alt="">
+            <hr>
+            <div class="name-price">
+                <h3 class="name">Car Name</h3>
+                <div class="price">Price / month</div>
+            </div>
+            <button>Rent Now</button>
+        </div>
+        <div class="car-details">
+            <img src="assets/images/car.jpg" alt="">
+            <hr>
+            <div class="name-price">
+                <h3 class="name">Car Name</h3>
+                <div class="price">Price / month</div>
+            </div>
+            <button>Rent Now</button>
+        </div>
+        <div class="car-details">
+            <img src="assets/images/car.jpg" alt="">
+            <hr>
+            <div class="name-price">
+                <h3 class="name">Car Name</h3>
+                <div class="price">Price / month</div>
+            </div>
+            <button>Rent Now</button>
+        </div>
+        <div class="car-details">
+            <img src="assets/images/car.jpg" alt="">
+            <hr>
+            <div class="name-price">
+                <h3 class="name">Car Name</h3>
+                <div class="price">Price / month</div>
+            </div>
+            <button>Rent Now</button>
+        </div>
+        <div class="car-details">
+            <img src="assets/images/car.jpg" alt="">
+            <hr>
+            <div class="name-price">
+                <h3 class="name">Car Name</h3>
+                <div class="price">Price / month</div>
+            </div>
+            <button>Rent Now</button>
+        </div>
+        <div class="car-details">
+            <img src="assets/images/car.jpg" alt="">
+            <hr>
+            <div class="name-price">
+                <h3 class="name">Car Name</h3>
+                <div class="price">Price / month</div>
+            </div>
+            <button>Rent Now</button>
+        </div>
+        <div class="car-details">
+            <img src="assets/images/car.jpg" alt="">
+            <hr>
+            <div class="name-price">
+                <h3 class="name">Car Name</h3>
+                <div class="price">Price / month</div>
+            </div>
+            <button>Rent Now</button>
+        </div>
+        <div class="car-details">
+            <img src="assets/images/car.jpg" alt="">
+            <hr>
+            <div class="name-price">
+                <h3 class="name">Car Name</h3>
+                <div class="price">Price / month</div>
+            </div>
+            <button>Rent Now</button>
+        </div>
+        <div class="car-details">
+            <img src="assets/images/car.jpg" alt="">
+            <hr>
+            <div class="name-price">
+                <h3 class="name">Car Name</h3>
+                <div class="price">Price / month</div>
+            </div>
+            <button>Rent Now</button>
+        </div>
+        <div class="car-details">
+            <img src="assets/images/car.jpg" alt="">
+            <hr>
+            <div class="name-price">
+                <h3 class="name">Car Name</h3>
+                <div class="price">Price / month</div>
+            </div>
+            <button>Rent Now</button>
+        </div>
+        <div class="car-details">
+            <img src="assets/images/car.jpg" alt="">
+            <hr>
+            <div class="name-price">
+                <h3 class="name">Car Name</h3>
+                <div class="price">Price / month</div>
+            </div>
+            <button>Rent Now</button>
+        </div>
+        <div class="car-details">
+            <img src="assets/images/car.jpg" alt="">
+            <hr>
+            <div class="name-price">
+                <h3 class="name">Car Name</h3>
+                <div class="price">Price / month</div>
+            </div>
+            <button>Rent Now</button>
         </div>
     </div>
 </div>
