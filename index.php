76bc25ea79e9f212f212b1ef6cc06494cc91f5a1
jennermaxim <jennersi1remaxim@gmail.com>
2024-03-17 11:00:19 +0300

    Add the Styles files and the car container in the home page

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-<?php include 'include/header.php'; ?>
+<?php include 'includes/header.php'; ?>
 
-<div class="login">
-    <form action="">
-        <input type="image" src="" alt="">
-    </form>
+<div class="home">
+    <div class="cars-container">
+        <div class="car-details">
+            <img src="assets/images/car.jpg" alt="">
+        </div>
+    </div>
 </div>
 
 <?php include 'include/footer.php'; ?>
