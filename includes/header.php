6e1a1bb456b82c0b42ba5e9d758e95e62548407e
jennermaxim <jennersi1remaxim@gmail.com>
2024-03-17 14:43:38 +0300

    Add the Top Bar

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,18 @@
     <title>MyCAR</title>
     <link rel="stylesheet" href="assets/styles/style.css">
     <link rel="stylesheet" href="assets/styles/home.css">
+    <link rel="stylesheet" href="assets/styles/header.css">
+
 </head>
 
 <body>
+    <div class="header">
+        <div class="logo">LOGO</div>
+        <div class="middle-nav">
+            <a href="#">About Us</a>
+        </div>
+        <div class="right-nav">
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+        </div>
+    </div>
